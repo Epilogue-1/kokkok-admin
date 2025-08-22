ad552b5d47067d9825ff656fc38fbe2414777bdc
heony704 <heony704@gmail.com>
2025-08-22 16:38:57 +0900

    chore: Pretendard 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "콕콕 어드민",
   description: "특별히 발탁된 정예 관리자들을 위한 콕콕 어드민 웹",
 };
+
+const pretendard = localFont({
+  src: "../fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+});
 
 export default function RootLayout({
   children,
@@ -23,12 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html lang="ko" className={pretendard.className}>
+      <body>{children}</body>
     </html>
   );
 }
