efaf44975c6da027c27b31606d15e38625819349
heony704 <heony704@gmail.com>
2025-09-05 22:38:05 +0900

    feat: 홈 진입 시, 사용자 신고 페이지로 리다이렉트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,5 @@
-import Header from "@/components/Header";
-import Main from "@/components/Main";
-import Title from "@/components/Title";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <Main>
-        <Title>콕콕 어드민에 오신 걸 환영합니다</Title>
-
-        <p>
-          이곳은 대시보드로 사용하거나, 사용안하고 바로 사용자 신고로 이동할
-          예정입니다.
-        </p>
-        <p>만약 대시보드 만든다면, 현재 확인해야 하는 신고/문의 띄워줄듯</p>
-      </Main>
-    </>
-  );
+  redirect("/user-reports");
 }
