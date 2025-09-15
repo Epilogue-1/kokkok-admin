1452b7801488b70f556e12f3c19d3fe31cea8f46
heony704 <heony704@gmail.com>
2025-09-15 16:32:36 +0900

    chore: next 이미지 도메인에 supabase 및 구글 소셜 추가

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,22 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
+      // Supabase 프로젝트
+      {
+        protocol: "https",
+        hostname: "zrkselfyyqkkqcmxhjlt.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
       {
         protocol: "https",
         hostname: "omrikgqmembehcfnvsce.supabase.co",
         pathname: "/storage/v1/object/public/**",
+      },
+      // 소셜 로그인 아바타(구글)
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
       },
     ],
   },
