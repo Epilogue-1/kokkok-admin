fbc245330903365d4067e6a85f680ffe59718914
heony704 <heony704@gmail.com>
2026-02-26 03:44:31 +0900

    remove: Table 컴포넌트가 쓰이지 않아 삭제

--- src/components/Table/TableHeadItem.tsx
+++
@@ -1,7 +0,0 @@
-interface Props extends React.PropsWithChildren {
-  className?: string;
-}
-
-export default function TableHeadItem({ className = "", children }: Props) {
-  return <th className={`${className} w-full leading-tight`}>{children}</th>;
-}
